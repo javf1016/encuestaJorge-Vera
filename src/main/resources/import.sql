5d34ab8dd4573ecc0a2b92840cd8cbb092289422
javf1016 <ja.vera@uniandes.edu.co>
2022-01-08 03:05:43 -0500

    Jorge Vera
    Configure postgres

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,5 +7,5 @@ INSERT INTO encuestas (id, numero_documento, email, comentarios, fecha_respuesta
 
 /* Creamos algunos usuarios */
 /*  admin pass = admin, user pass = user */
-INSERT INTO usuarios (username, password, enabled, rol) VALUES ('admin', '$2a$10$5Wwzdrr4m6bKbSuiPRVIUOhdNyUNY.fU5aX/IHhx.sumAd3AbYAsK', 1, 'ROLE_USER');
-INSERT INTO usuarios (username, password, enabled, rol) VALUES ('user', '$2a$10$plrCf4vExyWxIAY6hNpWN.McGpAox4GYM68Xe15hVOqf28WX8Fvay', 1, 'ROLE_ADMIN');
+INSERT INTO usuarios (username, password, enabled, rol) VALUES ('admin', '$2a$10$5Wwzdrr4m6bKbSuiPRVIUOhdNyUNY.fU5aX/IHhx.sumAd3AbYAsK', true, 'ROLE_USER');
+INSERT INTO usuarios (username, password, enabled, rol) VALUES ('user', '$2a$10$plrCf4vExyWxIAY6hNpWN.McGpAox4GYM68Xe15hVOqf28WX8Fvay', true, 'ROLE_ADMIN');
